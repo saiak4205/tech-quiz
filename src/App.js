25675029d795db7838887650c6416cb7459dd08f
saiak4205 <sai.ak4205@gmail.com>
2023-07-24 14:42:51 +0900

    途中保存(APIキー取得まで)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Top from "./screens/Top";
+import NavBar from "./components/NavBar";
+import Quiz from "./screens/Quiz";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path = "/" element={<Top />} />
+          <Route path = "/quiz" element={<Quiz />} />
+          <Route render = { () => <h4>not found...</h4>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
